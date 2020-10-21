da11c704926304a5741969ee85e84cfdbdd52e02
dai-bao-hua <1625397016@qq.com>
2020-10-21 19:05:05 +0800

    第一期

---
+++ src/main/java/com/dbh/shiro/config/main.java
@@ -0,0 +1,14 @@
+package com.dbh.shiro.config;
+
+/**
+ * @version 1.0
+ * @author：戴宝华
+ * @copyright: 阿里巴巴
+ * @Date: 2020/10/21 19:04
+ **/
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("hello github");
+    }
+}
